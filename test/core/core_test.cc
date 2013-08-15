3b2442c60ee333b263e1e69a0c1ed6f02f888d89
Tim Peters <mail@darksecond.nl>
2013-08-15 19:53:37 +0200

    added a test for the fnv1a hash

--- test/core/core_test.cc
+++ test/core/core_test.cc
@@ -1,9 +1,20 @@
 #include <cassert>
 #include <iostream>
 
-//#include <core/container/types.h>
-//#include <core/container/dynamic_array.h>
+#include <core/hashing/fnv1a.h>
+
+template <uint64_t NUM>
+struct test_compile_time_const {
+	static const uint64_t value = NUM;
+};
+
+void test_fnv1a_hash() {
+	assert(core::hash_fnv1a("test") == 18007334074686647077ULL);
+	//test for real compile time hash
+	test_compile_time_const<core::hash_fnv1a("test")> hash;
+}
 
 int main() {
+	test_fnv1a_hash();
 	return 0;
 }
