dc54060e1c8394d03a84e12f7207206ce032b3ec
Tim Peters <mail@darksecond.nl>
2014-03-04 10:39:15 +0100

    fixed a little

--- src/core/memory/stack_allocator.cc
+++ src/core/memory/stack_allocator.cc
@@ -14,6 +14,8 @@ void* core::stack_allocator::allocate(size_t size, const size_t alignment, size_
     size += sizeof(uint32_t);
     alignment_offset += sizeof(uint32_t);
 
+    const uint32_t allocation_offset = static_cast<uint32_t>(_current - _start);
+
     // Align _current
     _current = (uint8_t*)align_up(_current + alignment_offset, alignment) - alignment_offset;
     
@@ -33,7 +35,6 @@ void* core::stack_allocator::allocate(size_t size, const size_t alignment, size_
 
     as_char = _current;
 
-    const uint32_t allocation_offset = static_cast<uint32_t>(_current - _start);
     *as_uint32_t = allocation_offset;
     as_char += sizeof(uint32_t);
 
